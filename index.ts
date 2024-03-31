35f3ec151bdd019efd712365059a277c1810a6c2
Fazan Arshad <faizanarshad0101@gmail.com>
2024-03-31 15:25:32 +0500

    my first commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+let favoriteMovie: string = "The Lord of the Rings";
+
+let FirstName = "Fazan";
+let LastName = "Arshad";
+let age = 34;
+console.log(`Hello, my name is ${FirstName} and I am ${age} year old.`);
+
+const Happy: boolean = true;
+
+console.log(`Full Name: ${FirstName} ${LastName}`);
+
+let userAge: number = 34;
+
+let productName = "Headphones";
+let price = 99.99;
+
+console.log(`${productName} - Price: $${(99.99).toFixed(2)}`);
+
+let numPets: number = 2;
+
+let isBirthday: boolean = false;
+
+console.log(
+  `User Information: First Name: ${FirstName} Last Name: ${LastName} Age: ${34}`
+);
+
+const birthYear: number = 1989;
+
+let city = "multan";
+let country = "pakistan";
+console.log(`${city}, ${country}.`);
+
+let temperature: number = 29;
+
+console.log(`Today's Temperature: ${29}°C`);
